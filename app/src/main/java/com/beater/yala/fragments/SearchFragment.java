d39fe99729c58f9995065b9992afa35cb99ebaae
juanhuertab <juancarloshuertabarzola@gmail.com>
2018-06-16 22:03:15 -0500

    search fragment y spinner añadido

--- app/src/main/java/com/beater/yala/fragments/SearchFragment.java
+++ app/src/main/java/com/beater/yala/fragments/SearchFragment.java
@@ -3,21 +3,35 @@ package com.beater.yala.fragments;
 
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.beater.yala.ContainerActivity;
 import com.beater.yala.R;
 
 import com.beater.yala.model.Album;
 import com.loopj.android.http.*;
 
 import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.sql.Struct;
 import java.util.ArrayList;
@@ -27,10 +41,12 @@ import cz.msebera.android.httpclient.Header;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SearchFragment extends Fragment {
+public class SearchFragment extends Fragment implements Response.Listener<JSONObject>, Response.ErrorListener {
 
     private AsyncHttpClient cliente;
     private Spinner albumSpinner;
+    RequestQueue request;
+    JsonObjectRequest jsonObjectRequest;
 
     public SearchFragment() {
         // Required empty public constructor
@@ -40,10 +56,21 @@ public class SearchFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_search, container, false);
-        cliente = new AsyncHttpClient();
-        Spinner spinner =  (Spinner) view.findViewById(R.id.spinner_albums);
-
+        showToolbar(getResources().getString(R.string.Buscar_Figuritas_toolbar_title),false, view);
+        albumSpinner =  (Spinner) view.findViewById(R.id.spinner_albums);
+        request = Volley.newRequestQueue(getContext());
+        loadWebService();
         return view;
+    }
+
+    private void loadWebService() {
+        SharedPreferences preferences = getContext().getSharedPreferences("credenciales", Context.MODE_PRIVATE);
+        Integer idUser = preferences.getInt("id",1);
+
+        String url = "https://juanhb.000webhostapp.com/Obtener_Albumes_By_Usuario.php?idUser="+ idUser +"" ;
+        jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,url,null, this, this);
+        request.add(jsonObjectRequest);
+
     }
 
     private void llenaSpinner(){
@@ -53,35 +80,43 @@ public class SearchFragment extends Fragment {
 
         String url = "https://juanhb.000webhostapp.com/Obtener_Albumes_By_Usuario.php?idUser="+ idUser +"" ;
 
-        cliente.get(url, new AsyncHttpResponseHandler() {
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-                if(statusCode== 200){
-                    cargarSpinner(new String(responseBody));
+    }
+    @Override
+    public void onResponse(JSONObject response) {
+        ArrayList<Album> lista = new ArrayList<Album>();
+        JSONArray json = response.optJSONArray("albumes");
+        JSONObject jsonObject;
+
+        try {
+
+            for(int i=0;i<json.length();i++) {
+                jsonObject = json.getJSONObject(i);
+                String nombreAlbum =jsonObject.optString("titulo");
+                Album a = new Album();
+                a.setAlbumName(nombreAlbum);
+                lista.add(a);
                 }
-            }
 
-            @Override
-            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+            ArrayAdapter<Album> a = new ArrayAdapter<Album>(getActivity().getApplicationContext(), R.layout.spinner_item,lista);
+            albumSpinner.setAdapter(a);
 
-            }
-        });
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
     }
 
-    private void cargarSpinner(String respuesta) {
-        ArrayList<Album> lista = new ArrayList<Album>();
+    @Override
+    public void onErrorResponse(VolleyError error) {
 
-        try{
-            JSONArray json = new JSONArray(respuesta);
-            for (int i = 0 ; i < respuesta.length(); i++){
-                Album a = new Album();
-            }
+    }
 
-        }catch(Exception e){
-
-        }
-
+    public void showToolbar(String title, boolean upButton, View view){
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar);
+        toolbar.setTitleTextColor(Color.WHITE);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(title);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
     }
 
 }
